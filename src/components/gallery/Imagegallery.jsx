c3b02ca30ac51997cc4e091c5fce14506479841a
didipta <66532706+didipta@users.noreply.github.com>
2023-10-31 23:52:37 +0600

    imagegallaery done

--- src/components/gallery/Imagegallery.jsx
+++ src/components/gallery/Imagegallery.jsx
@@ -1,24 +1,57 @@
 import React, { useState } from "react";
 import Sortables from "./Sortable";
+import { data } from "@/utils/data";
 
 const Imagegallery = () => {
-  const [images, setImages] = useState([
-    "image-1.webp",
-    "image-2.webp",
-    "image-3.webp",
-    "image-4.webp",
-    "image-5.webp",
-    "image-6.webp",
-    "image-7.webp",
-    "image-8.webp",
-    "image-9.webp",
-    "image-10.jpeg",
-    "image-11.jpeg",
-  ]);
+  const [images, setImages] = useState(data);
+  const [clicked, setClicked] = useState([]);
+
+  const handleClick = (id) => {
+    console.log(id);
+    if (clicked.includes(id)) {
+      setClicked(clicked.filter((item) => item !== id));
+    } else {
+      setClicked([...clicked, id]);
+    }
+  };
+
+  const handleDelete = () => {
+    setImages(images.filter((item) => !clicked.includes(item.id)));
+    setClicked([]);
+    console.log(images);
+  };
+
   return (
     <div className="w-full">
-      <div className="w-9/12 mx-auto">
-        <Sortables images={images} />
+      <div className="w-full md:w-9/12 m-auto shadow-lg p-5 bg-white">
+        <div className=" border-b-2">
+          {clicked.length > 0 ? (
+            <div className="flex justify-between items-center mb-5">
+              <h1 className="text-xl font-bold">
+                <input
+                  type="checkbox"
+                  className="w-4 h-4 mx-2"
+                  defaultChecked={clicked.length !== 0 ? true : false}
+                />
+                {clicked.length} Selected Images
+              </h1>
+              <button
+                className="bg-red-500 text-white px-3 py-1 rounded-lg"
+                onClick={handleDelete}
+              >
+                Delete Files
+              </button>
+            </div>
+          ) : (
+            <h1 className="text-xl font-bold mb-5">Image Gallery</h1>
+          )}
+        </div>
+        <Sortables
+          images={images}
+          handleClick={handleClick}
+          clicked={clicked}
+          setImages={setImages}
+        />
       </div>
     </div>
   );
--- src/components/gallery/Sortable.jsx
+++ src/components/gallery/Sortable.jsx
@@ -2,41 +2,63 @@ import Image from "next/image";
 import React, { useRef, useEffect } from "react";
 import Sortable from "sortablejs";
 
-const Sortables = ({ images }) => {
+const Sortables = ({ images, handleClick, clicked, setImages }) => {
   const gridRef = useRef(null);
 
   useEffect(() => {
     const sortable = new Sortable(gridRef.current, {
       animation: 150,
     });
-  }, []);
 
+   
+  }, []);
   return (
     <div
       ref={gridRef}
-      className=" grid grid-cols-5 justify-between items-center gap-3"
+      className=" grid md:grid-cols-5 grid-cols-2 justify-between items-center gap-3 m-5"
     >
-      {images.map((image, index) => (
-        <div
-          key={index}
-          className=" border border-spacing-3 text-center first:col-span-2 first:row-span-2 first:h-full group relative"
-          data-id={index}
-        >
-          <Image
-            src={`/asset/images/${image}`}
-            width={500}
-            height={500}
-            alt=""
-            className="object-cover"
-          />
-          <div className="absolute inset-0 bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 transition duration-300 flex justify-center items-center">
-            <input
-              type="checkbox"
-              class="opacity-0 absolute top-3 left-3 group-hover:opacity-100 z-50 p-5 w-5 h-5"
+      {images.map((item, index) => (
+        <>
+          <div
+            key={index}
+            className=" rounded-lg border-2 text-center first:col-span-2 first:row-span-2 first:h-full group relative bg-slate-400 overflow-hidden"
+            data-id={index}
+          >
+            <Image
+              src={`/asset/images/${item?.image}`}
+              width={500}
+              height={500}
+              alt=""
+              className="object-cover bg-white"
             />
+            <div className="absolute inset-0 bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 transition duration-300 flex justify-center items-center">
+              <input
+                type="checkbox"
+                class="opacity-0 absolute top-3 left-3 group-hover:opacity-100 z-50 p-5 w-5 h-5"
+                onClick={() => handleClick(item?.id)}
+                checked={clicked.includes(item?.id)}
+              />
+            </div>
+            {clicked.includes(item?.id) && (
+              <input
+                type="checkbox"
+                defaultChecked
+                className="absolute top-3 left-3 z-0 p-5 w-5 h-5"
+              />
+            )}
           </div>
-        </div>
+        </>
       ))}
+      <div className=" rounded-lg border-dashed border-2 bo text-center overflow-hidden">
+        {" "}
+        <Image
+          src={`/asset/images/image12.webp`}
+          width={500}
+          height={500}
+          alt=""
+          className="object-cover bg-white"
+        />
+      </div>
     </div>
   );
 };
